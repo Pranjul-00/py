665d127e17dff3cb210b1a33ac73b36f80183849
Pranjul Gupta <pranjul.here@gmail.com>
2025-10-20 22:03:45 +0530

    3d slicing

--- learning-numpy.py
+++ learning-numpy.py
@@ -119,3 +119,19 @@ import numpy as np
 # print(arr[1,-3::-2])    #gives 8,6
 
 #--------------------------------------------------------
+
+# 3d array slice
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(arr[0, 0, 0])     #gives 1
+print(arr[0, 0, 1])     #gives 2
+print(arr[0, 0, 2])     #gives 3
+print(arr[0, 1, 0])     #gives 4
+print(arr[0, 1, 1])     #gives 5
+print(arr[0, 1, 2])     #gives 6
+print(arr[1, 0, 0])     #gives 7
+print(arr[1, 0, 1])     #gives 8
+print(arr[1, 0, 2])     #gives 9
+print(arr[1, 1, 0])     #gives 10
+print(arr[1, 1, 1])     #gives 11
+print(arr[1, 1, 2])     #gives 12
